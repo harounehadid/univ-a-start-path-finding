cc57aef4ad4f909f8836351d371694bb1274459f
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2023-03-03 10:41:18 +0100

    Update cell types object

--- src/features/sand-types/sandTypes.js
+++ src/features/sand-types/sandTypes.js
@@ -1,10 +1,28 @@
 const cellTypes = {
-    empty: 'empty',
-    grass: 'grass',
-    sand: 'sand',
-    water: 'water',
-    wall: 'wall',
-    goal: 'goal'
+    empty: {
+        name: 'empty',
+        cost: 1
+    },
+    grass: {
+        name: 'grass',
+        cost: 2
+    },
+    sand: {
+        name: 'sand',
+        cost: 4
+    },
+    water: {
+        name: 'water',
+        cost: 4
+    },
+    wall: {
+        name: 'wall',
+        cost: Infinity
+    },
+    goal: {
+        name: 'goal',
+        cost: 0
+    }
 }
 
 export default cellTypes;
